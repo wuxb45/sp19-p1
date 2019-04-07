dbd078bab02df76f511791bf4b0b6b25a9c92ed6
Wu Xingbo <wuxb45@gmail.com>
2019-04-06 21:19:06 -0500

    add xxhash

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ bpmain.out : bpmain.c bptree.c bptree.h
 	gcc $(OPT) -o bpmain.out bpmain.c bptree.c
 bemain.out : bemain.c betree.c betree.h
 	gcc $(OPT) -o bemain.out bemain.c betree.c
-odbmain.out : odbmain.c odb.c odb.h bptree.c bptree.h
-	gcc $(OPT) -o odbmain.out odbmain.c odb.c bptree.c
+odbmain.out : odbmain.c odb.c odb.h bptree.c bptree.h xxhash.c xxhash.h xxh3.h
+	gcc $(OPT) -o odbmain.out odbmain.c odb.c bptree.c xxhash.c
 
 clean :
 	rm -f *.out
--- odb.c
+++ odb.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <stdint.h>
 #include "odb.h"
 #include "bptree.h"
+
+#include "xxhash.h"
+typedef uint64_t u64; // use unsigned for hash values
+// // example of how to efficiently compute multiple hash value for bloom filters
+// // A similar approach is also used by LevelDB:
+// //    https://github.com/google/leveldb/blob/master/util/bloom.cc
+// const u64 hash0 = XXH64(buffer, length, 0);
+// const u64 hash1 = (hash0 >> 17) | (hash0 << 47);
+// const u64 hash2 = (hash1 >> 17) | (hash1 << 47);
+// // hash3 ... hash6
 
 // TODO: add helper functions when necessary
 
